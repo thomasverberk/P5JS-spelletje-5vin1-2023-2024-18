ce630a2cd2eea1e7fca22595c13724fa6930a7ab
thomasverberk <143184572+thomasverberk@users.noreply.github.com>
2023-09-08 11:59:57 +0000

    kever

--- script.js
+++ script.js
@@ -1,64 +1,24 @@
-var xJager = 50;
-var yJager = 50;
-var xProoi = 800;
-var yProoi = 175;
+var kater,katerKlein,landschap,kever;
+var keverX;
+var keverY;
+
+function preload() {
+  kater = loadImage("images/brieck.jpg");
+  bomen = loadImage("images/bomen.jpg");
+  kever = loadImage("images/sprites/kever.png");
+}
 
 function setup() {
-  canvas = createCanvas(100,400);
+  canvas = createCanvas(450,450);
   canvas.parent();
-  textFont("Verdana");
-  textSize(140);
-  noStroke();
-  frameRate(50);
+  keverX = 150;
+  keverY = 100;
 }
-
+ 
 function draw() {
-  background('olive');
-  if (keyIsDown(LEFT_ARROW)) {
-    xJager -= 5;
-  }
-  if (keyIsDown(RIGHT_ARROW)) {
-    xJager += 5;
-  }
-  if (keyIsDown(UP_ARROW)) {
-    yJager -= 5;
-  }
-  if (keyIsDown(DOWN_ARROW)) {
-    yJager += 5;
-  }
-  if (keyIsDown(65)) {
-    xProoi -= 7; 
-  }
-  if (keyIsDown(68)) {
-    xProoi += 7;
-  }
-  if (keyIsDown(87)) {
-    yProoi -= 7;
-  }
-  if (keyIsDown(83)) {
-    yProoi += 7;
-  }
-  
-  xJager = constrain(xJager,0,width - 100);
-  yJager = constrain(yJager,0,height - 100);
-  xProoi = constrain(xProoi,0,width - 75);
-  yProoi = constrain(yProoi,0,height - 50);
-
-  if (xJager >= xProoi-100 && xJager <= xProoi + 75 && yJager >= yProoi - 100 && yJager <= yProoi + 50) {
-    fill('chartreuse');
-    eindScherm();
-  }
-  else {
-    fill('darkkhaki');
-  }
-  rect(xProoi,yProoi,75,50);
-  fill('moccasin');
-  rect(xJager,yJager,100,100);   
-}
-
-function eindScherm() {
-  background('white');
-  fill('black');
-  text("GEVANGEN!",75,250);
-  noLoop();
+  background('grey');  
+  image(kater,25,25,400,400);
+  keverX+=random(-5,5);
+  keverY+=random(-5,5);
+  image(kever,keverX,keverY,30,30);
 }
